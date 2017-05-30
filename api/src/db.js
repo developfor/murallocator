81ae3b0a738cad872ada78871e812a6cbeac3607
Teja N <teja.s.naraparaju@gmail.com>
2017-05-30 09:44:13 -0400

    Upgrading the connection code for mongoose

--- api/src/db.js
+++ api/src/db.js
@@ -7,17 +7,20 @@ import envConfig from '../config/config';
 
 export default (callback) => {
   const mongoUri = envConfig.mongo.host;
-  mongoose.connect(mongoUri, { server: { socketOptions: { keepAlive: 1 } } });
   mongoose.Promise = bluebird;
-  mongoose.connection.on('error', () => {
-    throw new Error(`unable to connect to database: ${mongoUri}`);
-  });
 
-  if (envConfig.mongooseDebug) {
-    mongoose.set('debug', (collectionName, method, query, doc) => {
-      debug(`${collectionName}.${method}`, util.inspect(query, false, 20), doc);
-    });
-  }
-  // connect to a database if needed, then pass it to `callback`:
-  callback();
+  mongoose.connect(mongoUri, { server: { socketOptions: { keepAlive: 1 } } }).then(
+    () => {
+      if (envConfig.mongooseDebug) {
+        mongoose.set('debug', (collectionName, method, query, doc) => {
+          debug(`${collectionName}.${method}`, util.inspect(query, false, 20), doc);
+        });
+      }
+      callback();
+    },
+    (err) => {
+      console.log(err);
+      throw new Error(`unable to connect to database: ${mongoUri}`);
+    },
+  );
 };
