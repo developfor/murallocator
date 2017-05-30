7d661771ffa5cb3b199975f15a863600bc23e60a
Teja N <teja.s.naraparaju@gmail.com>
2017-05-30 09:58:19 -0400

    Making changes to config

--- api/config/config.js
+++ api/config/config.js
@@ -2,27 +2,17 @@ import Joi from 'joi';
 
 require('dotenv').config();
 
-const DB_CONFIG = {
-  development: {
-    HOST: 'mongodb://localhost:27017/murallocator-dev',
-    port: 27017,
-  },
-  test: {
-    HOST: 'mongodb://localhost:27017/murallocator-test',
-    port: 27017,
-  },
-  production: {
-    HOST: 'mongodb://localhost:27017/murallocator',
-    port: 27017,
-  },
-};
-
 const envVarsSchema = Joi.object({
   NODE_ENV: Joi.string()
     .allow(['development', 'production', 'test'])
     .default('development'),
   PORT: Joi.number()
     .default(3001),
+  MONGO_HOST: Joi.string()
+    .default('mongodb://localhost:27017/murallocator')
+    .description('Mongo DB host url'),
+  MONGO_PORT: Joi.number()
+    .default(27017),
   MONGOOSE_DEBUG: Joi.boolean()
     .when('NODE_ENV', {
       is: Joi.string().equal('development'),
@@ -36,6 +26,22 @@ const { error, value: envVars } = Joi.validate(process.env, envVarsSchema);
 if (error) {
   throw new Error(`Config validation error: ${error.message}`);
 }
+
+
+const DB_CONFIG = {
+  development: {
+    HOST: 'mongodb://localhost:27017/murallocator-dev',
+    port: 27017,
+  },
+  test: {
+    HOST: 'mongodb://localhost:27017/murallocator-test',
+    port: 27017,
+  },
+  production: {
+    HOST: envVarsSchema.MONGO_HOST,
+    port: envVarsSchema.MONGO_PORT,
+  },
+};
 
 const config = {
   env: envVars.NODE_ENV,
