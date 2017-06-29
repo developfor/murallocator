9f89f664923475b59c76d966e446298b95d5c9f7
Teja N <teja.s.naraparaju@gmail.com>
2017-06-28 20:33:42 -0400

    Porting over the header code and css

--- client/src/components/layout/Header/Header.js
+++ client/src/components/layout/Header/Header.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 
 import './Header.css';
 
 class Header extends Component {
   render() {
     return (
-      <div>
-        <link href="https://fonts.googleapis.com/css?family=Lato|Roboto:300,400,500" rel="stylesheet"/>
-        <h1>Title</h1>
+      <div className="header-bar">
+        <div className="header-left">
+          <Link className="no-style-link" to="/">
+            <img src="/images/logo.svg" alt="Mural Locator Logo" />
+          </Link>
+        </div>
+        <div className="header-right">
+          <a className="no-style-link" href="#">
+            <img src="/images/menu.svg" alt="Menu" width="25" />
+          </a>
+        </div>
       </div>
     );
   }
