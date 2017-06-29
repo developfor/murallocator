4ab0ab69b65a5fa5e117e564adf73f9eed1ecf8c
Teja N <teja.s.naraparaju@gmail.com>
2017-06-28 20:38:40 -0400

    Adding Skeleton.css to the app

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import Header from '../components/layout/Header/Header'
 import 'normalize.css/normalize.css';
+import 'skeleton-css/css/skeleton.css';
 import './App.css';
 
 class App extends Component {
